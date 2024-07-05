9bf8a599339702b3d3a95bc401c68240dae75e94
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-07-05 12:59:15 -0700

    7-5-24

--- curriculum/level-6/auth-lesson-project/client/src/components/Form.jsx
+++ curriculum/level-6/auth-lesson-project/client/src/components/Form.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState, UserContext } from 'react';
+
 
 function Form(props) {
 
-    const {isMember} = props
 
-    return ( 
-     
-            <form name = 'auth-form' id = 'auth-form'>
 
-                <h2>Welcome to RTV!</h2>
-                <input placeholder='username' />
-                <input placeholder='password' />
-                <button>{isMember ? "Login" : "Signup"}</button>
+    const initState = {
+        username: '',
+        password: ''
+    }
 
-            </form>
-       
-     );
+    const [formData, setFormData] = useState(initState)
+
+    const { isMember, submit } = props
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit(e) {
+
+        e.preventDefault()
+        submit(formData)
+        
+    }
+
+    return (
+
+        <form name='auth-form' id='auth-form' onSubmit={handleSubmit}>
+
+            <h2>Welcome to RTV!</h2>
+            <input placeholder='username' name='username' value={formData.username} onChange={handleChange} />
+            <input placeholder='password' name='password' value={formData.password} onChange={handleChange} />
+            <button>{isMember ? "Login" : "Signup"}</button>
+
+        </form>
+
+    );
 }
 
 export default Form;
